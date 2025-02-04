dc2974970f8552ece58d2184e27dac469a1d1cf3
Wesley Cho <57739900+wes-cho@users.noreply.github.com>
2025-02-03 20:38:44 -0500

    Add date formatter function

--- src/index.js
+++ src/index.js
@@ -9,6 +9,32 @@ const todayTest = createTodoItem("Today's Task", "2025-02-03", "", "Medium", "Th
 const tomorrowTest = createTodoItem("Tomorrow's Task", "2025-02-04", "", "Low", "This is a test todo item for tomorrow");
 const somedayTest = createTodoItem("Someday Task", "2026-03-01", "", "High", "This is a test todo item for someday");
 
+function dateFormatter(dateInstance){
+    const date = new Date(dateInstance);
+    const year = date.getFullYear();
+    var month = date.getMonth()+1;    
+    var day = date.getDate();
+
+    if (month < 10 && day < 10){
+        month = `0${month}`;
+        day = `0${day}`;
+
+        return `${year}-${month}-${day}`;
+
+    } else if (month < 10 && day > 10){
+        month = `0${month}`;
+
+        return `${year}-${month}-${day}`;
+
+    } else if (month > 10 && day < 10){
+        day = `0${day}`;
+
+        return `${year}-${month}-${day}`;
+    } else {
+        return `${year}-${month}-${day}`
+    };
+};
+
 const inboxButton = document.querySelector("#inbox");
 inboxButton.addEventListener("click", () => {    
     content.replaceChildren();
